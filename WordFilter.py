e36c26283224e491552a3fce55d09b961cf70288
George Mohammad <38092421+GeorgeMohammad@users.noreply.github.com>
2019-10-01 13:27:27 -0500

    Removed some Whitespace.

--- WordFilter.py
+++ WordFilter.py
@@ -8,17 +8,11 @@ wholeChat = nps_chat.posts('10-19-20s_706posts.xml')
 
 
 #wholeChat = [i for i in chat]
-
 #based on geeksforgeeks code
 wholeChat = [j for i in wholeChat for j in i if j not in stopwords.words("english")]
-
-
 minSize = 10
 wholeChat = [i for i in wholeChat if len(i) >= minSize]
-
 chatFreqDist = nltk.FreqDist(wholeChat)
-
 mostCommonNum = 10
 commonWords = chatFreqDist.most_common(mostCommonNum)
-
 print(commonWords)
